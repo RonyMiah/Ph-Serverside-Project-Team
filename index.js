0a40debaed459e0a287501360dc616056b290900
RonyMiah <ronymiah2121@gmail.com>
2022-02-10 20:53:51 +0600

    server side start

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Backend Team project !')
+})
+
+
+app.listen(port, () => {
+    console.log(` Listening on port ${port}`)
+})
