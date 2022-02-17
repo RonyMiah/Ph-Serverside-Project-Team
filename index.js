554de69250f3316b6daaed9a154dab564ced4854
RonyMiah <ronymiah2121@gmail.com>
2022-02-17 12:53:39 +0600

    admin role added

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ async function run() {
         await client.connect();
         const database = client.db("DhakaShop");
         const productCollection = database.collection("all_product");
-        console.log('Database connected successfully');
+        const userCollection = database.collection('users')
+
+        // console.log('Database connected successfully');
 
         app.post('/addService', async (req, res) => {
             const service = req.body;
@@ -30,9 +32,51 @@ async function run() {
             console.log(result);
 
             res.json(result)
-        } )
+        })
+
+        app.post('/users', async (req, res) => {
+
+            const users = req.body;
+            const result = await userCollection.insertOne(users);
+            res.json(result)
+        })
+        
+        
+        // app.post('/addReview', async (req, res) => {
+        //     const service = req.body;
+        //     const result = await productCollection.insertOne(service);
+
+        //     console.log(result);
+
+        //     res.json(result)
+        // } )
 
         // get method
+
+
+
+
+        // Update User ......... Upsert User 
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await userCollection.updateOne(filter,updateDoc, options );
+            res.json(result)
+        })
+
+        // users add Admin role
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set:{ role: 'admin'} };
+            const result = await userCollection.updateOne(filter,updateDoc );
+            res.json(result)
+        })
+
 
         app.get('/product', async (req, res) => {
             const cursor = productCollection.find({});
